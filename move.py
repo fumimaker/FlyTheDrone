47604bb2fbe2da8e2bdf2a318103d31b164d1f6f
fumi <fumi01200924@gmail.com>
2020-02-15 16:40:28 +0900

    Update move.py

--- move.py
+++ move.py
@@ -23,21 +23,9 @@ parser = argparse.ArgumentParser(
 parser.add_argument('--connect',
                     help="Vehicle connection target string. If not specified, SITL automatically started and used.")
 args = parser.parse_args()
-
 connection_string = args.connect
 sitl = None
 
-
-#Start SITL if no connection string specified
-if not connection_string:
-    import dronekit_sitl
-    sitl = dronekit_sitl.start_default()
-    connection_string = sitl.connection_string()
-
-
-# Connect to the Vehicle
-print('Connecting to vehicle on: %s' % connection_string)
-vehicle = connect(connection_string, wait_ready=True)
 
 
 def arm_and_takeoff(aTargetAltitude):
@@ -74,38 +62,8 @@ def arm_and_takeoff(aTargetAltitude):
         time.sleep(1)
 
 
-#Arm and take of to altitude of 5 meters
-arm_and_takeoff(5)
 
-
-"""
-Convenience functions for sending immediate/guided mode commands to control the Copter.
-
-The set of commands demonstrated here include:
-* MAV_CMD_CONDITION_YAW - set direction of the front of the Copter (latitude, longitude)
-* MAV_CMD_DO_SET_ROI - set direction where the camera gimbal is aimed (latitude, longitude, altitude)
-* MAV_CMD_DO_CHANGE_SPEED - set target speed in metres/second.
-
-
-The full set of available commands are listed here:
-http://dev.ardupilot.com/wiki/copter-commands-in-guided-mode/
-"""
-
-
-def condition_yaw(heading, relative=False):
-    """
-    Send MAV_CMD_CONDITION_YAW message to point vehicle at a specified heading (in degrees).
-
-    This method sets an absolute heading by default, but you can set the `relative` parameter
-    to `True` to set yaw relative to the current yaw heading.
-
-    By default the yaw of the vehicle will follow the direction of travel. After setting 
-    the yaw using this function there is no way to return to the default yaw "follow direction 
-    of travel" behaviour (https://github.com/diydrones/ardupilot/issues/2427)
-
-    For more information see: 
-    http://copter.ardupilot.com/wiki/common-mavlink-mission-command-messages-mav_cmd/#mav_cmd_condition_yaw
-    """
+def condition_yaw(heading, relative=True):
     if relative:
         is_relative = 1  # yaw relative to direction of travel
     else:
@@ -124,24 +82,7 @@ def condition_yaw(heading, relative=False):
     vehicle.send_mavlink(msg)
 
 
-
 def send_ned_velocity(velocity_x, velocity_y, velocity_z, duration):
-    """
-    Move vehicle in direction based on specified velocity vectors and
-    for the specified duration.
-
-    This uses the SET_POSITION_TARGET_LOCAL_NED command with a type mask enabling only 
-    velocity components 
-    (http://dev.ardupilot.com/wiki/copter-commands-in-guided-mode/#set_position_target_local_ned).
-    
-    Note that from AC3.3 the message should be re-sent every second (after about 3 seconds
-    with no message the velocity will drop back to zero). In AC3.2.1 and earlier the specified
-    velocity persists until it is canceled. The code below should work on either version 
-    (sending the message multiple times does not cause problems).
-    
-    See the above link for information on the type_mask (0=enable, 1=ignore). 
-    At time of writing, acceleration and yaw bits are ignored.
-    """
     msg = vehicle.message_factory.set_position_target_local_ned_encode(
         0,       # time_boot_ms (not used)
         0, 0,    # target system, target component
@@ -160,21 +101,6 @@ def send_ned_velocity(velocity_x, velocity_y, velocity_z, duration):
 
 
 def send_global_velocity(velocity_x, velocity_y, velocity_z, duration):
-    """
-    Move vehicle in direction based on specified velocity vectors.
-
-    This uses the SET_POSITION_TARGET_GLOBAL_INT command with type mask enabling only 
-    velocity components 
-    (http://dev.ardupilot.com/wiki/copter-commands-in-guided-mode/#set_position_target_global_int).
-    
-    Note that from AC3.3 the message should be re-sent every second (after about 3 seconds
-    with no message the velocity will drop back to zero). In AC3.2.1 and earlier the specified
-    velocity persists until it is canceled. The code below should work on either version 
-    (sending the message multiple times does not cause problems).
-    
-    See the above link for information on the type_mask (0=enable, 1=ignore). 
-    At time of writing, acceleration and yaw bits are ignored.
-    """
     msg = vehicle.message_factory.set_position_target_global_int_encode(
         0,       # time_boot_ms (not used)
         0, 0,    # target system, target component
@@ -198,134 +124,26 @@ def send_global_velocity(velocity_x, velocity_y, velocity_z, duration):
         time.sleep(1)
 
 
-"""
-Fly a triangular path using the standard Vehicle.simple_goto() method.
-
-The method is called indirectly via a custom "goto" that allows the target position to be
-specified as a distance in metres (North/East) from the current position, and which reports
-the distance-to-target.
-"""
+#Start SITL if no connection string specified
+if not connection_string:
+    import dronekit_sitl
+    sitl = dronekit_sitl.start_default()
+    connection_string = sitl.connection_string()
 
 
-"""
-Fly the vehicle in a SQUARE path using velocity vectors (the underlying code calls the 
-SET_POSITION_TARGET_LOCAL_NED command with the velocity parameters enabled).
+# Connect to the Vehicle
+print('Connecting to vehicle on: %s' % connection_string)
+vehicle = connect(connection_string, wait_ready=True)
 
-The thread sleeps for a time (DURATION) which defines the distance that will be travelled.
-
-The code also sets the yaw (MAV_CMD_CONDITION_YAW) using the `set_yaw()` method in each segment
-so that the front of the vehicle points in the direction of travel
-"""
-
-
-#Set up velocity vector to map to each direction.
-# vx > 0 => fly North
-# vx < 0 => fly South
-NORTH = 2
-SOUTH = -2
-
-# Note for vy:
-# vy > 0 => fly East
-# vy < 0 => fly West
-EAST = 2
-WEST = -2
-
-# Note for vz:
-# vz < 0 => ascend
-# vz > 0 => descend
-UP = -0.5
-DOWN = 0.5
-
+#Arm and take of to altitude of 5 meters
+arm_and_takeoff(5)
 
 # Square path using velocity
-print("SQUARE path using SET_POSITION_TARGET_LOCAL_NED and velocity parameters")
+print("Yaw +45 relative")
+condition_yaw(45, relative=True)
 
-print("Yaw 180 absolute (South)")
-condition_yaw(180)
-
-print("Velocity South & up")
-send_ned_velocity(SOUTH, 0, UP, DURATION)
-send_ned_velocity(0, 0, 0, 1)
-
-
-print("Yaw 270 absolute (West)")
-condition_yaw(270)
-
-print("Velocity West & down")
-send_ned_velocity(0, WEST, DOWN, DURATION)
-send_ned_velocity(0, 0, 0, 1)
-
-
-print("Yaw 0 absolute (North)")
-condition_yaw(0)
-
-print("Velocity North")
-send_ned_velocity(NORTH, 0, 0, DURATION)
-send_ned_velocity(0, 0, 0, 1)
-
-
-print("Yaw 90 absolute (East)")
-condition_yaw(90)
-
-print("Velocity East")
-send_ned_velocity(0, EAST, 0, DURATION)
-send_ned_velocity(0, 0, 0, 1)
-
-
-"""
-Fly the vehicle in a DIAMOND path using velocity vectors (the underlying code calls the 
-SET_POSITION_TARGET_GLOBAL_INT command with the velocity parameters enabled).
-
-The thread sleeps for a time (DURATION) which defines the distance that will be travelled.
-
-The code sets the yaw (MAV_CMD_CONDITION_YAW) using the `set_yaw()` method using relative headings
-so that the front of the vehicle points in the direction of travel.
-
-At the end of the second segment the code sets a new home location to the current point.
-"""
-
-print("DIAMOND path using SET_POSITION_TARGET_GLOBAL_INT and velocity parameters")
-# vx, vy are parallel to North and East (independent of the vehicle orientation)
-
-print("Yaw 225 absolute")
-condition_yaw(225)
-
-print("Velocity South, West and Up")
-send_global_velocity(SOUTH, WEST, UP, DURATION)
-send_global_velocity(0, 0, 0, 1)
-
-
-print("Yaw 90 relative (to previous yaw heading)")
-condition_yaw(90, relative=True)
-
-print("Velocity North, West and Down")
-send_global_velocity(NORTH, WEST, DOWN, DURATION)
-send_global_velocity(0, 0, 0, 1)
-
-print("Set new home location to current location")
-vehicle.home_location = vehicle.location.global_frame
-print("Get new home location")
-#This reloads the home location in DroneKit and GCSs
-cmds = vehicle.commands
-cmds.download()
-cmds.wait_ready()
-print(" Home Location: %s" % vehicle.home_location)
-
-
-print("Yaw 90 relative (to previous yaw heading)")
-condition_yaw(90, relative=True)
-
-print("Velocity North and East")
-send_global_velocity(NORTH, EAST, 0, DURATION)
-send_global_velocity(0, 0, 0, 1)
-
-
-print("Yaw 90 relative (to previous yaw heading)")
-condition_yaw(90, relative=True)
-
-print("Velocity South and East")
-send_global_velocity(SOUTH, EAST, 0, DURATION)
-send_global_velocity(0, 0, 0, 1)
+print("Yaw +180 relative")
+condition_yaw(180, relative=True)
 
 
 """
