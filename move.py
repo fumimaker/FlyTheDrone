497f4249ac0e88b4fdd1dae82d2aa5208232ad9b
fumi <fumi01200924@gmail.com>
2020-02-21 21:42:58 +0900

    Sim

--- move.py
+++ move.py
@@ -7,17 +7,8 @@ from pymavlink import mavutil  # Needed for command message definitions
 import time
 import math
 
-
-#Set up option parsing to get connection string
-import argparse
-parser = argparse.ArgumentParser(
-    description='Control Copter and send commands in GUIDED mode ')
-parser.add_argument('--connect',
-                    help="Vehicle connection target string. If not specified, SITL automatically started and used.")
-args = parser.parse_args()
-connection_string = args.connect
-sitl = "127.0.0.1:14549"
-
+connection_string = "127.0.0.1:14549"
+sitl = None
 
 def arm_and_takeoff(aTargetAltitude):
     """
@@ -115,29 +106,77 @@ def send_global_velocity(velocity_x, velocity_y, velocity_z, duration):
         time.sleep(1)
 
 
+def headingRelative(headRel):
+    
+    '''nowDeg = math.degrees(vehicle.attitude.yaw)
+    diff = abs(headRel - nowDeg)
+    while diff - abs(nowDeg-math.degrees(vehicle.attitude.yaw)) > diff*0.95:
+        print(math.degrees(vehicle.attitude.yaw))
+        time.sleep(1)'''
+    time.sleep(5)
+    print("Yaw heading completed.")
+
 #Start SITL if no connection string specified
 if not connection_string:
     import dronekit_sitl
     sitl = dronekit_sitl.start_default()
     connection_string = sitl.connection_string()
 
-
 # Connect to the Vehicle
 print('Connecting to vehicle on: %s' % connection_string)
 vehicle = connect(connection_string, wait_ready=True)
 
 #Arm and take of to altitude of 5 meters
-arm_and_takeoff(5)
+arm_and_takeoff(3)
+DURATION = 20  # Set duration for each segment.
+#Set up velocity vector to map to each direction.
+# vx > 0 => fly North
+# vx < 0 => fly South
+NORTH = 2
+SOUTH = -2
+
+# Note for vy:
+# vy > 0 => fly East
+# vy < 0 => fly West
+EAST = 2
+WEST = -2
+
+# Note for vz:
+# vz < 0 => ascend
+# vz > 0 => descend
+UP = -0.5
+DOWN = 0.5
 
 # Square path using velocity
-print("Yaw +45 relative")
-condition_yaw(45, relative=True)
+print("Yaw 180 absolute (South)")
+condition_yaw(180)
+print("Velocity South & up")
+send_ned_velocity(SOUTH, 0, UP, DURATION)
+send_ned_velocity(0, 0, 0, 1)
 
-print("Yaw +180 relative")
-condition_yaw(180, relative=True)
+print("Yaw 270 absolute (West)")
+condition_yaw(270)
+
+print("Velocity West & down")
+send_ned_velocity(0, WEST, DOWN, DURATION)
+send_ned_velocity(0, 0, 0, 1)
 
 
+print("Yaw 0 absolute (North)")
+condition_yaw(0)
 
+print("Velocity North")
+send_ned_velocity(NORTH, 0, 0, DURATION)
+send_ned_velocity(0, 0, 0, 1)
+
+
+print("Yaw 90 absolute (East)")
+
+condition_yaw(90)
+
+print("Velocity East")
+send_ned_velocity(0, EAST, 0, DURATION)
+send_ned_velocity(0, 0, 0, 1)
 """
 The example is completing. LAND at current location.
 """
